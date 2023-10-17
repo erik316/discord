880f58456609af98aae6d8bcc9927c1ae727151d
erik316 <144471001+erik316@users.noreply.github.com>
2023-10-17 19:25:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import discord
 from discord.ext import commands
-import os, random
-print(os.listdir('images'))
+import  random
+  
+tips = ["Dont throw trash outside', 'dont use a lot of plastic bags', 'dont throw away food', 'dont throw trash in the ocean, 'keep your yard clean that the trash from can be blown away and become litter', 'pick up trash from outside' "]
+ocean_pounds = ('33 billion pounds')
+total_pounds = ('4.5 trillion')
 import requests
 intents = discord.Intents.default()
 intents.message_content = True
@@ -11,84 +14,17 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+ 
+@bot.command()
+async def nature_tip(ctx):
+    await ctx.send(tips)
 
 @bot.command()
-async def hello(ctx):
-    await ctx.send(f'Hello! Im {bot.user}!')
+async def how_many_ocean_pounds(ctx):
+    await ctx.send(ocean_pounds)
 
 @bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
+async def how_many_pounds_in_the_world(ctx):
+    await ctx.send(total_pounds)
 
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
-@bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
-
-@bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
-
-@bot.group()
-async def cool(ctx):
-    """Says if a user is cool.
-
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
-
-@bot.command()
-async def repeat(ctx, times: int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await ctx.send(content) 
-
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
-
-@bot.command()
-async def mem(ctx):
-    img_name = random.choice(os.listdir('images'))
-    # А вот так можно подставить имя файла из переменной!
-    with open(f'images/{img_name}', 'rb') as f:
-        picture = discord.File(f)
-   # Можем передавать файл как параметр!
-    await ctx.send(file=picture)
-
-def get_duck_image_url():    
-    url = 'https://random-d.uk/api/random'
-    res = requests.get(url)
-    data = res.json()
-    return data['url']
-
-@bot.command('duck')
-async def duck(ctx):
-    '''По команде duck вызывает функцию get_duck_image_url'''
-    image_url = get_duck_image_url()
-    await ctx.send(image_url)
-
-def get_dog_image_url():    
-    url = 'https://random.dog/woof.json'
-    res = requests.get(url)
-    data = res.json()
-    return data['url']
-
-@bot.command('dog')
-async def dog(ctx):
-    '''По команде duck вызывает функцию get_duck_image_url'''
-    image_url = get_dog_image_url()
-    await ctx.send(image_url)
-
-bot.run("MTE1NjYxMjM4ODY5NTU3MjU0Mw.G_ChT9.0pNl2v00UTp-er8_kF_d4Zx4NzTQjdUsuuweTw")
-
+bot.run("MTE1NjYxMjM4ODY5NTU3MjU0Mw.G2fwW4.A31xZo7KLKAl56rvmrKU4G1bk-3YgjRb72yle8")
