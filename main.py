2b820c215f250d1dae32c532f49b06fcbf629b8a
erik316 <144471001+erik316@users.noreply.github.com>
2023-10-18 15:42:03 +0300

    added new commands to the bot

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import discord
 from discord.ext import commands
 import  random
   
-tips = ["Dont throw trash outside', 'dont use a lot of plastic bags', 'dont throw away food', 'dont throw trash in the ocean, 'keep your yard clean that the trash from can be blown away and become litter', 'pick up trash from outside' "]
+tips = ["Don't throw trash outside', 'dont use a lot of plastic bags', 'dont throw away food', 'dont throw trash in the ocean, 'keep your yard clean that the trash from can be blown away and become litter', 'pick up trash from outside' "]
 ocean_pounds = ('33 billion pounds')
-total_pounds = ('4.5 trillion')
+total_ocean_pounds = ('75 to 199 tons')
+total_pounds_per_day = ('3.5 million pounds')
+total_pounds = ('4.5 trillion tons')
 import requests
 intents = discord.Intents.default()
 intents.message_content = True
@@ -25,6 +27,14 @@ async def how_many_ocean_pounds(ctx):
 
 @bot.command()
 async def how_many_pounds_in_the_world(ctx):
+    await ctx.send(total_ocean_pounds)
+
+@bot.command()
+async def how_many_pounds_in_the_world(ctx):
     await ctx.send(total_pounds)
 
-bot.run("MTE1NjYxMjM4ODY5NTU3MjU0Mw.G2fwW4.A31xZo7KLKAl56rvmrKU4G1bk-3YgjRb72yle8")
+@bot.command()
+async def how_many_pounds_in_the_world_per_day(ctx):
+    await ctx.send(total_pounds_per_day)
+
+bot.run("Put your bots token here")
